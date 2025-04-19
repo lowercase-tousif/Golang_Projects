8d970094d6e80861d8d6aa9674bc2a050f1142f3
lowercase-tousif <toisiftasrik@gmail.com>
2025-04-19 16:27:03 +0600

    welcome message added in the game

--- NumberGuessingGame/main.go
+++ NumberGuessingGame/main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+// Welcome Message
+func welcomeMessage() {
+	fmt.Print("\n\n\t\t\t Welcome to the Simple Number Guessing Game\t\t\t\n\n")
+}
+
 func main() {
-	fmt.Println("Project Starting....")
+	welcomeMessage()
 }
