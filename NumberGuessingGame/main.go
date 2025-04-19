7f87e62932a5ea3d208551cacb7150a3ab0904f4
lowercase-tousif <toisiftasrik@gmail.com>
2025-04-19 17:37:18 +0600

    main game loop function added

--- NumberGuessingGame/main.go
+++ NumberGuessingGame/main.go
@@ -7,6 +7,34 @@ func welcomeMessage() {
 	fmt.Print("\n\n\t\t\t Welcome to the Simple Number Guessing Game\t\t\t\n\n")
 }
 
+func mainGameLoop() {
+
+	var attempt int = 0
+	var guess int
+	for {
+		fmt.Println("Enter your guess: ")
+		fmt.Scan(&guess)
+
+		attempt++
+
+		var secretNumber = ...
+
+		if guess < secretNumber {
+			fmt.Println("Too low try again!")
+		} else if guess > secretNumber {
+			fmt.Println("Too high try again!")
+		} else {
+			fmt.Println("You have successfully guessed the number✨")
+			break;
+		}
+
+		if attempt == 3{
+			fmt.Println("Game Ended")
+			break;
+		}
+	}
+}
+
 func main() {
 	welcomeMessage()
 }
