0094f56454d27b322c18881407d37a34b733f481
lowercase-tousif <toisiftasrik@gmail.com>
2025-04-19 18:27:10 +0600

    Game Completed

--- EvenOddChecker/main.go
+++ EvenOddChecker/main.go
@@ -6,6 +6,21 @@ func welcomeMessage() {
 	fmt.Print("\n\n\t\t\t Even Odd Checker Game \t\t\t\t\n\n")
 }
 
+func evenOddChecker() {
+	
+	var num int
+	
+	fmt.Print("Enter any number: ")
+	fmt.Scan(&num)
+
+	if num % 2 == 0 {
+		fmt.Print("\n\n\t\t\t\t Even Number ✨\t\t\t\t\n\n")
+	} else{
+		fmt.Print("\n\n\t\t\t\t Odd Number 🎉\t\t\t\t\n\n")
+	}
+}
+
 func main() {
 	welcomeMessage()
+	evenOddChecker()
 }
