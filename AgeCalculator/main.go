5a66bb23bdff1094a2af914399fefb86eeb4c0cc
lowercase-tousif <toisiftasrik@gmail.com>
2025-04-20 21:39:59 +0600

    Draft Age Calculator Completed

--- AgeCalculator/main.go
+++ AgeCalculator/main.go
@@ -1,12 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 // Welcome Message
 func welcomeMessage() {
 	fmt.Print("\n\n\t\t\t\t Age Calculator 📆\t\t\t\t\n\n")
 }
 
+// Calculate The BirthYear
+func calcYear() {
+	fmt.Print("Enter your birth year: ")
+
+	var userYear int
+	fmt.Scan(&userYear)
+
+	// getting the current year using time.Now().Year()
+	var currentYear int = time.Now().Year()
+
+	var result int = currentYear - userYear
+
+	fmt.Printf("\n\n\t\t\t\t You are %d years old.\t\t\t\t\n\n", result)
+
+}
 func main() {
 	welcomeMessage()
+	calcYear()
 }
