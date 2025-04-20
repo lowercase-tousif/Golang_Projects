fb022456c40b593d1ce20e14526d9c0914f699ce
lowercase-tousif <toisiftasrik@gmail.com>
2025-04-20 21:29:51 +0600

    Welcome message added into the code

--- AgeCalculator/main.go
+++ AgeCalculator/main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+// Welcome Message
+func welcomeMessage() {
+	fmt.Print("\n\n\t\t\t\t Age Calculator 📆\t\t\t\t\n\n")
+}
+
 func main() {
-	fmt.Println("Hello")
+	welcomeMessage()
 }
