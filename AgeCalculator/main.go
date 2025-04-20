e11633334c67430b07bc4cc9b7200ed844a29410
lowercase-tousif <toisiftasrik@gmail.com>
2025-04-20 21:52:29 +0600

    Age Calculator completed

--- AgeCalculator/main.go
+++ AgeCalculator/main.go
@@ -10,22 +10,44 @@ func welcomeMessage() {
 	fmt.Print("\n\n\t\t\t\t Age Calculator 📆\t\t\t\t\n\n")
 }
 
-// Calculate The BirthYear
-func calcYear() {
-	fmt.Print("Enter your birth year: ")
+func userInput() {
 
+	// Getting the user input separately like year , month and date
+	fmt.Print("Enter your birth year: ")
 	var userYear int
 	fmt.Scan(&userYear)
 
+	fmt.Print("Enter your birth Month: ")
+	var userMonth int
+	fmt.Scan(&userMonth)
+
+	fmt.Print("Enter your birth date: ")
+	var userDay int
+	fmt.Scan(&userDay)
+
+	// Calling the calcYear Function
+	calcYear(userYear, userMonth, userDay)
+}
+
+func calcYear(userYear int, userMonth int, userDay int) {
+
 	// getting the current year using time.Now().Year()
 	var currentYear int = time.Now().Year()
+	var currentMonth int = int(time.Now().Month())
+	var currentDay int = time.Now().Day()
 
+	// Calculating the result for the year
 	var result int = currentYear - userYear
 
-	fmt.Printf("\n\n\t\t\t\t You are %d years old.\t\t\t\t\n\n", result)
+	// Checking if the birthday appread this year or not
+	if (currentMonth < userMonth) || (currentMonth == userMonth && (currentDay < userDay)) {
+		result--
+	}
 
+	fmt.Printf("\n\n\t\t\t\t You are %d years old.\t\t\t\t\n\n", result)
 }
+
 func main() {
 	welcomeMessage()
-	calcYear()
+	userInput()
 }
